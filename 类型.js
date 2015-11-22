a17251755da85ae52afb42cdfbce0181bb468a31
侯小龙 <houxiaolong@yunyao-inc.com>
2015-11-23 00:07:44 +0800

    类型

---
+++ 类型.js
@@ -0,0 +1,12 @@
+// 特殊原始值：null undefined
+// 数字：64位的浮点数，不区分整型和浮点类型
+// Math对象，复杂的运算
+// Infinity NaN isNaN() isFinity()
+// Date
+var nowDate = new Date(2015, 9, 31);
+var nowTime = new Date(2015, 9, 31, 8, 39,48);
+var now = new Date();
+
+// 文本
+// Boolean
+// false: null undefined "" 0 -0 NaN
